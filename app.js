01c0690ad4163abbaca3263fa077a3b0ea3047c0
Emma Ritcey <email": null>
2016-10-26 18:26:43 -0500

    add lots of other cool javscript practice

--- app.js
+++ app.js
@@ -7,6 +7,28 @@ $(document).ready(function(){
 
   $("#blue").on("click", function(){
     $("body").css("background", "white");
-    $("body").append("<h1>HEY THERE</h1>" )
+    $("body").append("<h1>HEY THERE</h1>")
   })
+
+
 });
+
+
+function duplicate(array) {
+  var new_array = array.concat(array);
+  return new_array;
+}
+
+console.log(duplicate([1,2,3]))
+
+for(var i=0; i<100; i++) {
+  if(i%3 === 0 && i%5===0){
+    console.log("fizzbuzz");
+  }else if(i%5 === 0){
+    console.log("buzz");
+  }else if(i%3 === 0) {
+    console.log("fizz");
+  }
+}
+
+console.log("HI!")
